1797b62996c577a859ec100916eb7c862fff9273
Enrico Bertolazzi <enrico.bertolazzi@unitn.it>
2022-01-09 19:30:43 +0100

    updated binary for OSX

--- toolbox/bin/osx/filter_dylib.rb
+++ toolbox/bin/osx/filter_dylib.rb
@@ -4,7 +4,7 @@ require 'colorize'
 #
 ['libgfortran.5','libquadmath.0','libstdc++.6','libgcc_s.1','libipopt.3','libcoinmumps.3','libmetis'].each do |lib|
   system('install_name_tool -id "@loader_path/'+lib+'.dylib" '+lib+'.dylib');
-  ['libgfortran.5','libquadmath.0','libcoinmumps.3','libstdc++.6','libgcc_s.1'].each do |name|
+  ['libgfortran.5','libquadmath.0','libcoinmumps.3','libstdc++.6','libgcc_s.1','libmetis'].each do |name|
     path = `otool -L #{lib}.dylib | grep #{name}`;
     if path != "" then
       path.gsub!(/\s*([^\s]+).*/,'\1')
