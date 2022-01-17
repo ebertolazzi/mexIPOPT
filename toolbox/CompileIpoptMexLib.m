ebertolazzi/mexIPOPT

commit 944b74085ba7abf720e7a7b47c0fa0d59fe16b7b

Updated to IPOPT 3.14.4 for OSX (MATLAB+OCTAVE)

toolbox/CompileIpoptMexLib.m
@@ -5,36 +5,54 @@ old_dir = cd(fileparts(which(mfilename)));
 
 isOctave = isempty(ver('matlab'));
 
-if ~isOctave    % skip for Octave (which doesn't yet have INMEM)
+if isOctave % skip for Octave (which doesn't yet have INMEM)
+  if ismac
+    fprintf('Compile for OCTAVE on MACOSX\n');
+  else
+    fprintf('Compile for OCTAVE\n');
+  end
+else
   [~,mexLoaded] = inmem('-completenames');
   eval('while mislocked(''ipopt''); munlock(''ipopt''); end;');
 end
 
 disp('---------------------------------------------------------');
-CMD = 'mex -largeArrayDims -Isrc ';
-CMD = [ CMD ' ./src/ipopt.cc ./src/IpoptInterfaceCommon.cc '];
+SRC = ' ./src/ipopt.cc ./src/IpoptInterfaceCommon.cc ';
+CMD = [ 'mex -largeArrayDims -Isrc ' SRC ];
 
 if isOctave
-  MEX_EXE = 'bin/ipopt_oct';
-  [status, IPOPT_INCL] = system('pkg-config --cflags ipopt', 1);
-  [status, IPOPT_LIBS] = system('pkg-config --libs ipopt', 1);
-  CMD = [ CMD ' ' IPOPT_INCL(1:end-1) ...
-    ' -output ' MEX_EXE ' -Wall -O2 -g ' ...
-    IPOPT_LIBS(1:end-1) ...
-  ];
+  CMD = [ 'mkoctfile --mex ' SRC ' -Wall -O2 -g -Isrc '];
+  if ismac
+    %
+    % libipopt must be set with:
+    % install_name_tool -id "@loader_path/libipopt.3.dylib" libipopt.3.dylib
+    %
+    IPOPT_HOME = '../Ipopt';
+    CMD = [ CMD ...
+      '-I' IPOPT_HOME '/include_osx/coin-or ' ...
+      '-DOS_OCTAVE_ON_MAC -output bin/osx/ipopt_osx_octave ' ...
+      '-Lbin/osx -lcoinmumps.3 -lipopt.3 -lgfortran.5 -lquadmath.0 -lmetis -lstdc++.6 -lgcc_s.1 -ldl ' ...
+    ];
+  else
+    MEX_EXE = 'bin/ipopt_oct';
+    [status, IPOPT_INCL] = system('pkg-config --cflags ipopt', 1);
+    [status, IPOPT_LIBS] = system('pkg-config --libs ipopt', 1);
+    CMD = [ CMD ' ' IPOPT_INCL(1:end-1) ' -output ' MEX_EXE IPOPT_LIBS(1:end-1) ];
+  end
 elseif ismac
   %
   % libipopt must be set with:
   % install_name_tool -id "@loader_path/libipopt.3.dylib" libipopt.3.dylib
   %
+  %  ' bin/osx/libc++.1.dylib bin/osx/libc++abi.dylib bin/osx/libgcc_s.1.dylib' ...
   IPOPT_HOME = '../Ipopt';
   CMD = [ CMD ...
     '-I' IPOPT_HOME '/include_osx/coin-or ' ...
     '-DOS_MAC -output bin/osx/ipopt_osx ' ...
-    ' bin/osx/libipopt.3.dylib bin/osx/libcoinmumps.2.dylib ' ...
-    ' bin/osx/libgfortran.5.dylib bin/osx/libquadmath.0.dylib ' ...
-    ' bin/osx/libc++.1.dylib bin/osx/libc++abi.dylib bin/osx/libgcc_s.1.dylib' ...
-    'LDFLAGS=''$LDFLAGS -Wl,-rpath,. -framework Accelerate'' ' ...
+    ' bin/osx/libcoinmumps.3.dylib bin/osx/libipopt.3.dylib ' ...
+    ' bin/osx/libgfortran.5.dylib bin/osx/libquadmath.0.dylib bin/osx/libmetis.dylib ' ...
+    ' bin/osx/libstdc++.6.dylib bin/osx/libgcc_s.1.dylib' ...
+    'LDFLAGS=''$LDFLAGS -Wl,-rpath,. -framework Accelerate -ldl'' ' ...
     'CXXFLAGS=''$CXXFLAGS -Wall -O2 -g'' ' ...
   ];
   %%  'LDFLAGS=''$LDFLAGS -Wl,-rpath,./ -Lbin/osx -L/usr/local/lib -lipopt -lcoinmumps -lgfortran -lquadmath  -framework Accelerate'' ' ...

commit 9dc9595786e8c80f798041822d0f07d3df907768

Fix command to build Octave on Linux, broken in 944b740.

toolbox/CompileIpoptMexLib.m
@@ -49,7 +49,7 @@ if isOctave
     MEX_EXE = 'bin/ipopt_oct';
     [status, IPOPT_INCL] = system('pkg-config --cflags ipopt', 1);
     [status, IPOPT_LIBS] = system('pkg-config --libs ipopt', 1);
-    CMD = [ CMD ' ' IPOPT_INCL(1:end-1) ' -output ' MEX_EXE IPOPT_LIBS(1:end-1) ];
+    CMD = [ CMD IPOPT_INCL(1:end-1) ' -output ' MEX_EXE ' ' IPOPT_LIBS(1:end-1) ];
   end
 elseif ismac
   %